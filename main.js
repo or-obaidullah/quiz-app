caef99787fac33d34a1d13da02caf08eed9f8f96
or-obaidullah <obaidullah.cse.diit@gmail.com>
2021-04-17 13:53:16 +0600

    After some changes

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const quizDB = [
-    {
+const quizDB = [{
         question: 'Q1: What is the full form of "AI"?',
         a: 'Aeronautical Information',
         b: 'Accurate Information',
@@ -38,9 +37,10 @@ const quizDB = [
         c: 'All Through Market',
         d: 'Ascii Transmission Method',
         ans: 'ans2'
-    },   
+    },
 ];
-
+const startQuiz = document.getElementById('startQuiz');
+const innerDiv = document.getElementById('innerDiv');
 const question = document.getElementById('question');
 const option1 = document.getElementById('option-1');
 const option2 = document.getElementById('option-2');
@@ -50,12 +50,37 @@ const submit = document.getElementById('submit');
 const answers = document.querySelectorAll('.answer');
 const showScore = document.getElementById('showScore');
 
+startQuiz.addEventListener('click', () => {
+    innerDiv.style.display = 'block';
+    startQuiz.style.display = 'none';
+
+
+    let count = quizDB.length * 60;
+    timeSpan.innerHTML = `00:0${quizDB.length}:00`;
+    let interval = setInterval(() => {
+        let seconds = count % 60;
+        let minutes = Math.floor(count / 60);
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        timeSpan.innerHTML = `00:${minutes}:${seconds}`;
+        count--;
+        if (count === 0) {
+            timeSpan.innerHTML = `00:00:00`;
+            clearInterval(interval);
+        }
+    },1000);
+});
+
 let questionCount = 0;
 let score = 0;
 
-const loadQuestion = () =>{
+const loadQuestion = () => {
     const quesList = quizDB[questionCount];
-    question.innerText =quesList.question;
+    question.innerText = quesList.question;
     option1.innerText = quesList.a;
     option2.innerText = quesList.b;
     option3.innerText = quesList.c;
@@ -66,34 +91,48 @@ loadQuestion();
 const getCheckAnswer = () => {
     let answer;
     answers.forEach((element) => {
-        if(element.checked){
+        if (element.checked) {
             answer = element.id;
         }
     });
     return answer;
 };
 
-const deselectAll = () =>{
+const deselectAll = () => {
     answers.forEach(element => element.checked = false);
 }
 
-submit.addEventListener('click',() => {
+submit.addEventListener('click', () => {
     const checkAnswer = getCheckAnswer();
     console.log(checkAnswer);
-    if(checkAnswer === quizDB[questionCount].ans){
-        score++;
-    }; 
-    questionCount++;
-    deselectAll();
-    if(questionCount < quizDB.length){
-        loadQuestion();
-    }else{
-        
-        showScore.innerHTML=`<p>You score: ${score}/${quizDB.length} ✌</p>
+    if (checkAnswer == 'ans1' || checkAnswer == 'ans2' || checkAnswer == 'ans3' || checkAnswer == 'ans4' || checkAnswer == 'ans') {
+        if (checkAnswer === quizDB[questionCount].ans) {
+            score++;
+        };
+        questionCount++;
+        deselectAll();
+        if (questionCount < quizDB.length) {
+            loadQuestion();
+        } else {
+
+            showScore.innerHTML = `<p>You score: ${score}/${quizDB.length} ✌</p>
+            <button class="btn" onClick="location.reload()">Play Again</button>`;
+            showScore.className = 'scoreArea';
+            // submit.disabled = true;
+            submit.trigger('click');
+        }
+    } else if (timeSpan.innerHTML == '00:00:00') {
+        showScore.innerHTML = `<p>You score: ${score}/${quizDB.length} ✌</p>
         <button class="btn" onClick="location.reload()">Play Again</button>`;
-        showScore.className='scoreArea';
+        showScore.className = 'scoreArea';
         submit.disabled = true;
     }
-    
+
+
 });
 
+//time
+let timeSpan = document.getElementById('time-span');
+// let countDate = new Date('00:05:00').getTime();
+// let currentTime= new Date().getTime();
+// console.log(countDate );
